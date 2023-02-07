1e0ea14d8df624e7f2fed0b7fb1af1869357aa52
Nino Annighoefer <nino@ninoan.com>
2023-02-07 19:59:48 +0000

    Bin stuff

--- basics.v
+++ basics.v
@@ -246,3 +246,44 @@ Proof.
     rewrite orb_false_l in H.
     assumption.
 Qed.
+
+Inductive bin : Type :=
+  | Z
+  | B0 (n : bin)
+  | B1 (n : bin).
+
+
+Fixpoint incr (m:bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 rest => B1 rest
+  | B1 rest => B0 (incr rest)
+  end.
+
+Fixpoint bin_to_nat (m:bin) : nat :=
+  match m with
+  | Z => 0
+  | B1 rest => 1 + 2 * (bin_to_nat rest)
+  | B0 rest => 2 * (bin_to_nat rest)
+  end.
+
+Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
+Proof. easy. Qed.
+
+Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
+Proof. easy. Qed.
+
+Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
+Proof. easy. Qed.
+
+Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
+Proof. easy. Qed.
+
+Example test_bin_incr5 :
+        bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
+Proof. easy. Qed.
+
+Example test_bin_incr6 :
+        bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
+Proof. easy. Qed.
+
