9b7f1fcf2f7c1d8289b3d44a111fec5ff28ae29b
Nino Annighoefer <nino@ninoan.com>
2023-02-18 18:02:22 +0000

    Finish Lists chapter

--- Lists.v
+++ Lists.v
@@ -1,5 +1,6 @@
 Require Import Unicode.Utf8.
 Require Import List.
+From Learn Require Import Utils.
 
 Notation "x =? y" := (Nat.eqb x y) (at level 70) : nat_scope.
 Notation "x <=? y" := (Nat.leb x y) (at level 70) : nat_scope.
@@ -509,3 +510,94 @@ Proof.
   rewrite rev_involutive in H.
   assumption.
 Qed.
+
+(* Options *)
+
+Inductive natoption : Type :=
+  | Some (n : nat)
+  | None.
+
+Definition hd_error (l : natlist) : natoption :=
+  match l with
+  | nil => None
+  | h :: t => Some h
+  end.
+
+Example test_hd_error1 : hd_error [] = None.
+Proof. reflexivity. Qed.
+
+Example test_hd_error2 : hd_error [1] = Some 1.
+Proof. reflexivity. Qed.
+
+Example test_hd_error3 : hd_error [5;6] = Some 5.
+Proof. reflexivity. Qed.
+
+Definition option_elim (d : nat) (o : natoption) : nat :=
+  match o with
+  | Some n' => n'
+  | None => d
+  end.
+
+Theorem option_elim_hd : ∀ (l:natlist) (default:nat),
+  hd default l = option_elim default (hd_error l).
+Proof. intros. destruct l; reflexivity. Qed.
+
+(* Partial maps *)
+
+Inductive id : Type :=
+  | Id (n : nat).
+
+Definition eqb_id (x1 x2 : id) :=
+  match x1, x2 with
+  | Id n1, Id n2 => n1 =? n2
+  end.
+
+Theorem eqb_id_refl : ∀ x, eqb_id x x = true.
+Proof.
+  intros.
+  destruct x.
+  simpl.
+  rewrite eqb_with_self_if_true.
+  reflexivity.
+Qed.
+
+Inductive partial_map : Type :=
+  | empty
+  | record (i : id) (v : nat) (m : partial_map).
+
+Definition update (d : partial_map)
+                  (x : id) (value : nat)
+                  : partial_map :=
+  record x value d.
+
+Fixpoint find (x : id) (d : partial_map) : natoption :=
+  match d with
+  | empty => None
+  | record y v d' => if eqb_id x y
+                     then Some v
+                     else find x d'
+  end.
+
+Theorem update_eq :
+  ∀ (d : partial_map) (x : id) (v: nat),
+    find x (update d x v) = Some v.
+Proof.
+  intros.
+  simpl.
+  rewrite eqb_id_refl.
+  reflexivity.
+Qed.
+
+Theorem update_neq :
+  ∀ (d : partial_map) (x y : id) (o: nat),
+    eqb_id x y = false → find x (update d y o) = find x d.
+Proof.
+  intros.
+  simpl.
+  rewrite H.
+  reflexivity.
+Qed.
+
+Inductive baz : Type :=
+  | Baz1 (x : baz)
+  | Baz2 (y : baz) (b : bool).
