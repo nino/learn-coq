6cde63671cb8ce5d0adc1d1a96c50db65f44ac9a
Nino Annighoefer <nino@ninoan.com>
2023-02-21 12:51:33 +0000

    Example

--- Poly.v
+++ Poly.v
@@ -225,5 +225,8 @@ Fixpoint flatten {X : Type} (l: list (list X)) : list X :=
 Definition flat_map {X Y: Type} (f: X → list Y) (l: list X) : list Y :=
   flatten (map f l).
 
-
+Example test_flat_map1:
+  flat_map (fun n => [n;n;n]) [1;5;4]
+  = [1; 1; 1; 5; 5; 5; 4; 4; 4].
+Proof. reflexivity. Qed.
 
