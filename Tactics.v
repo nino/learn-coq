b372d6d5d10847a69842fd174f1ebee1948ba4a4
Nino Annighoefer <nino@ninoan.com>
2023-02-22 21:27:54 +0000

    Equality is transitive

--- Tactics.v
+++ Tactics.v
@@ -54,4 +54,32 @@ Proof.
   reflexivity.
 Qed.
 
+Example trans_eq_example: forall (a b c d e f : nat),
+  [a; b] = [c; d] ->
+  [c; d] = [e; f] ->
+  [a; b] = [e; f].
+Proof.
+  intros.
+  rewrite H.
+  apply H0.
+Qed.
+
+Theorem trans_eq: forall (X:Type) (n m o: X),
+  n = m -> m = o -> n = o.
+Proof.
+  intros X n m o eq1 eq2.
+  rewrite eq1.
+  apply eq2.
+Qed.
+
+Example trans_eq_example': forall (a b c d e f : nat),
+  [a; b] = [c; d] ->
+  [c; d] = [e; f] ->
+  [a; b] = [e; f].
+Proof.
+  intros a b c d e f eq1 eq2.
+  apply trans_eq with (m := [c; d]).
+  - apply eq1.
+  - apply eq2.
+Qed.
 
