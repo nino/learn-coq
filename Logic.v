1268cd6792d643061e2a4d4e33d0336896037e0b
Nino Annighoefer <nino@ninoan.com>
2023-02-28 11:14:48 +0000

    and_example

--- Logic.v
+++ Logic.v
@@ -1,4 +1,9 @@
+Require Import Unicode.Utf8.
+
 Definition plus_claim : Prop := 2 + 2 = 4.
 
 Theorem plus_claim_is_true : plus_claim.
 Proof. reflexivity. Qed.
+
+Example and_example : 3 + 4 = 7 ∧ 2 * 2 = 4.
+Proof. split; reflexivity. Qed.
