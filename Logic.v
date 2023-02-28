bf225d57e4de3fd3893f734a34de7bbd2d20804a
Nino Annighoefer <nino@ninoan.com>
2023-02-28 15:55:47 +0000

    and_exercise

--- Logic.v
+++ Logic.v
@@ -1,4 +1,6 @@
 Require Import Unicode.Utf8.
+Require Import Nat.
+Require Import PeanoNat.
 
 Definition plus_claim : Prop := 2 + 2 = 4.
 
@@ -7,3 +9,32 @@ Proof. reflexivity. Qed.
 
 Example and_example : 3 + 4 = 7 ∧ 2 * 2 = 4.
 Proof. split; reflexivity. Qed.
+
+Lemma and_intro : ∀ A B : Prop, A → B → A ∧ B.
+Proof.
+  intros A B HA HB. split.
+  - apply HA.
+  - apply HB.
+Qed.
+
+Example and_example' : 3 + 4 = 7 ∧ 2 * 2 = 4.
+Proof. apply and_intro; reflexivity. Qed.
+
+Example and_exercise : forall n m : nat, n + m = 0 -> n = 0 /\ m = 0.
+Proof.
+  intros. split.
+  - destruct m as [| m'] eqn:mEq.
+    + rewrite Nat.add_0_r in H.
+      apply H.
+    + destruct n.
+      * discriminate H.
+      * discriminate H.
+  - destruct n.
+    + simpl in H. apply H.
+    + destruct m.
+      * reflexivity.
+      * simpl in H.
+        destruct (n + S m); discriminate.
+Qed.
+
+
